d4b2f52a46d4851d6ef833c7ec553f5e35f35f9b
Al Audet <alaudet@linuxnorth.org>
2014-05-19 19:24:10 -0400

    timelapse.py
    Add option to use minutes or seconds for the timelapse interval

--- scripts/timelapse.py
+++ scripts/timelapse.py
@@ -3,9 +3,18 @@
 import time
 import picamera
 
-print "How many minutes would you like your photo interval to be?"
-minutes = int(raw_input(">: "))
-seconds = minutes * 60
+method = raw_input("Select timelapse in seconds or minutes (S or M):> ")
+
+if "s" in method or "S" in method:
+    seconds = int(raw_input("Enter photo interval in seconds: >: "))
+
+elif "m" in method or "M" in method:
+    minutes = int(raw_input("Enter photo internval in minutes: >: "))
+    seconds = minutes * 60
+
+else:
+    print "Invalid entry"
+    exit(0)
 
 with picamera.PiCamera() as camera:
     camera.start_preview()
